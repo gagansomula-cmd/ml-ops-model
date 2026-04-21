961720f36cd29c546f3f3524a62e54960f7aa677
gagansomula-cmd <gagansomula@gmail.com>
2026-04-21 17:14:22 +0530

    Revert "fix: Correct access_scope syntax in EKS access policy"
    This reverts commit 41e44427ee4449c155cefe29a7759a8f94d115ec.

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -219,13 +219,10 @@ resource "aws_eks_access_entry" "github_actions" {
 }
 
 resource "aws_eks_access_policy_association" "github_actions" {
-  cluster_name  = aws_eks_cluster.main.name
-  principal_arn = aws_iam_role.github_actions.arn
-  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-
-  access_scope {
-    type = "cluster"
-  }
+  cluster_name       = aws_eks_cluster.main.name
+  principal_arn      = aws_iam_role.github_actions.arn
+  access_scope_type  = "cluster"
+  policy_arn         = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
   depends_on = [aws_eks_access_entry.github_actions]
 }
