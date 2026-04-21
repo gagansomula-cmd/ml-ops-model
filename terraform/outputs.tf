0262fb0f6a976e58586afa2e2997b5f6dd1e3420
gagansomula-cmd <gagansomula@gmail.com>
2026-04-21 10:51:37 +0530

    new cluster

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,8 @@
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = aws_eks_cluster.main.name
+}
+
 output "cluster_id" {
   description = "EKS cluster ID"
   value       = aws_eks_cluster.main.id
